f3c23fef2736e9909cce78ffc2aa986de042c365
Ali Masood <ali@zenex.co.jp>
2019-07-13 17:33:07 +0900

    Adds file interface

---
+++ lib/file.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace J\FS;
+
+interface File
+{
+
+    public function dir(): string;
+
+    public function name(): string;
+
+    public function fullPath(): string;
+
+    public function size(): int;
+
+    public function extension(): string;
+
+    public function mime(): string;
+
+}
