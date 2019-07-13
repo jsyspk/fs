ceda0a3352c6c160008dd8d4a2e2c61aca869c46
Ali Masood <ali@zenex.co.jp>
2019-07-13 21:19:27 +0900

    Adds readable file with test cases

--- lib/any-file.php
+++ lib/any-file.php
@@ -18,46 +18,62 @@ abstract class AnyFile implements File
 
     public function __construct(string $file)
     {
+        if(empty($file))
+        {
+            throw new InvalidArgumentException("Given file name '$file'  is empty", 20000);
+        }
+        if(is_dir($file))
+        {
+            throw new InvalidArgumentException("Given file path '$file' is not a file but it is a directory", 20001);
+        }
         if(!is_file($file))
         {
-            throw new InvalidArgumentException("Given name $file is not a valid file", 20001);
+            throw new InvalidArgumentException("Given file '$file' doesn't exist", 20002);
         }
-        $this->getInfo();
+
         $this->subjectFile = $file;
     }
 
     public function dir(): string
     {
+        $this->getInfo();
         return $this->dirName;
     }
 
     public function extension(): string
     {
+        $this->getInfo();
         return $this->extension;
     }
 
     public function size(): int
     {
+        $this->getInfo();
         // TODO: Implement size() method.
     }
 
     public function name(): string
     {
+        $this->getInfo();
         return $this->fullName;
     }
 
     public function coreName(): string
     {
+        $this->getInfo();
         return $this->coreName;
     }
 
-    private function getInfo()
+    private function getInfo(): bool
     {
-        $fileInfo = pathinfo($this->subjectFile);   //     /www/htdocs/inc/lib.inc.php
-        $this->dirName = $fileInfo['dirname'];      //     /www/htdocs/inc
-        $this->fullName = $fileInfo['basename'];    //     lib.inc.php
-        $this->extension = $fileInfo['extension'];  //     php
-        $this->coreName = $fileInfo['filename'];    //     lib.inc
+        if(!$this->fullName){
+            $fileInfo = pathinfo($this->subjectFile);   //     /www/htdocs/inc/lib.inc.php
+            $this->dirName = $fileInfo['dirname'];      //     /www/htdocs/inc
+            $this->fullName = $fileInfo['basename'];    //     lib.inc.php
+            $this->extension = $fileInfo['extension'];  //     php
+            $this->coreName = $fileInfo['filename'];    //     lib.inc
+        }
+        return true;
     }
 
     public function mime(): string
--- lib/readable-file.php
+++ lib/readable-file.php
@@ -6,4 +6,12 @@ namespace J\FS;
 class ReadableFile extends AnyFile
 {
 
+    public function __construct(string $file)
+    {
+        parent::__construct($file);
+        if(!is_readable($file))
+        {
+            throw new \InvalidArgumentException("Given file '$file' is not accessible. Please check permissions", 20003);
+        }
+    }
 }
